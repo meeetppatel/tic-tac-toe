caefe121a86dc136cd17b4b29c672704c2c1e498
ptlmeet27 <20124040@nuv.ac.in>
2022-07-19 01:22:08 +0530

    complete pvp mode

--- script.js
+++ script.js
@@ -3,28 +3,40 @@ const boxes = document.querySelectorAll(".divs");
 const container = document.querySelector(".container");
 const restartbtn = document.getElementById("restart-btn");
 
+container.style.visibility = "hidden";
+
 restartbtn.addEventListener("click", () => {
-  window.location.reload();
-  roundreset();
+  game.roundreset();
   gameboard.reset();
+  game.playround();
+  game.updategameboard();
 });
 
 // display controller
 
 const displaycontroller = (() => {
-  const messageElement = document.getElementById("message");
+  const pvpmode = document.getElementById("Player");
+  const aimode = document.getElementById("ailevels");
+  const mainmenu = document.getElementById("Menu");
 
+  pvpmode.addEventListener("click", () => {
+    container.style.visibility = "visible";
+    aimode.style.visibility = "hidden";
+    gameboard.reset();
+  });
+
+  mainmenu.addEventListener("click", () => {
+    window.location.reload();
+  });
+
+  const messageElement = document.getElementById("message");
 
   const setMessage = (message) => {
     messageElement.textContent = message;
   };
 
-
   return { setMessage };
 })();
-
-
-
 
 // gameboard
 const gameboard = (() => {
@@ -46,49 +58,45 @@ const gameboard = (() => {
     }
   };
 
-  return { setField, getField, reset ,board};
+  return { setField, getField, reset, board };
 })();
-
-
-
 
 // game
 const game = (() => {
   let round = 1;
   let isover = false;
 
-
   boxes.forEach((box) => {
     box.textContent = "";
     box.addEventListener("click", (e) => {
+      if (isover) return;
       playround(parseInt(e.target.dataset.index));
       updategameboard();
       console.log(round);
     });
   });
 
-
   const playround = (index) => {
-    gameboard.setField(index,getcurrentplayersign());
-    if(checkwinner(boxes)){
+    gameboard.setField(index, getcurrentplayersign());
+    if (checkwinner(boxes)) {
       displaycontroller.setMessage(`Player ${getcurrentplayersign()} Won !`);
       isover = true;
       return;
-    }if(round >= 9){
+    }
+    if (round >= 9) {
       displaycontroller.setMessage("Its'a Tie !");
-      isover= true;
+      isover = true;
       return;
     }
     round++;
     displaycontroller.setMessage(`Player ${getcurrentplayersign()}'s turn`);
-  }
+  };
 
   const updategameboard = () => {
     for (let i = 0; i < boxes.length; i++) {
       boxes[i].textContent = gameboard.getField(i);
     }
-  }
-
+  };
 
   const roundreset = () => {
     round = 0;
@@ -117,4 +125,6 @@ const game = (() => {
       });
     });
   };
+
+  return { round, isover, roundreset, playround, updategameboard };
 })();
